76dbcff49fa5642e11c33acf577c959804f612ad
LARS BERG <larsgbr@gmail.com>
2017-10-17 09:53:58 -0500

    Created Digital Clock and associated files

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class TickTock extends React.Component{
+
+	constructor(props){
+		super(props);
+		this.state = {date: new Date()};
+	}
+
+
+	componentDidMount(){
+		this.timerID = setInterval(
+		() => this.tick(),
+		1000
+		);	
+	}
+
+	componentWillMount(){
+		clearInterval(this.timerID);
+	}
+	tick() {
+		this.setState({
+			date:new Date()
+		});
+	}
+
+	render(){
+		return(
+			<div>
+				<h1>{this.state.date.toLocaleTimeString()}</h1>
+			</div>
+			);
+	}
+
+}
+
+ReactDOM.render(<TickTock />, document.getElementById('root'));
+
